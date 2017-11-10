f0db5ba0fa433b7ad8e7717ad8f73037b916b00e
TedHan <t.han0922@gmail.com>
2017-11-10 17:21:17 +0800

    init phenotype results controller

---
+++ Controllers/PhenotypeResultsController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Phenotype.Models;
+using Newtonsoft.Json.Linq;
+
+namespace Phenotype.Controllers
+{
+    [Route("api/[controller]")]
+    public class PhenotypeResultsController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public PhenotypeResultsController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpPost("batch")]
+        public async Task<IActionResult> CreateBatch([FromBody] JObject json)
+        {
+            var father = _dataContext.Genotypes.FirstOrDefault(g => g.Value == json.Value<string>("father"));
+            var mother = _dataContext.Genotypes.FirstOrDefault(g => g.Value == json.Value<string>("mother"));
+
+            List<JToken> values = json["values"].ToList();
+            List<PhenotypeResult> listToReturn = new List<PhenotypeResult>();
+
+            foreach (var item in values)
+            {
+                var tmp = new PhenotypeResult
+                {
+                    Id = Guid.NewGuid(),
+                    Father = father,
+                    Mother = mother,
+                    Probability = item.Value<Double>("prob"),
+                    Result = item.Value<string>("result")
+                };
+                _dataContext.PhenotypeResults.Add(tmp);
+                listToReturn.Add(tmp);
+            }
+
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(new RequestResult
+            {
+                Stat = RequestStatus.Success,
+                Data = listToReturn
+            });
+        }
+    }
+}
